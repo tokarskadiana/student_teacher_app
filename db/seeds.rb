a27e85fb8be5fda4787698122982ec9dd69b97b1
tokarskadiana <tokarskadiana@com>
2016-12-15 19:01:28 +0100

    Add seeds for payment

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,23 +11,24 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    last_name: Faker::Name.last_name,
+    birthdate: Faker::Date.backward(rand(5000...10000)
   )
 end
 
 students = Student.all
 SubjectItem.all.each do |subject_item|
-  subject_item.students << students.sample(rand(1..4))
+  subject_item.students << students.sample(rand(1..25))
 end
 
 SubjectItem.all.each do |subject_item|
@@ -37,6 +38,12 @@ SubjectItem.all.each do |subject_item|
                                                                 subject_item: subject_item,
                                                                 value: rand(1..6))
     end
+  end
+end
+
+Student.all.each do |student|
+  rand(3..8).times do
+    Payment.create(value: rand(900..1500), date: Faker::Date.backward(rand(1...300)), student: student)
   end
 end
 
